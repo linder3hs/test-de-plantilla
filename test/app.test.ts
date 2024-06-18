2424bfe7905eb58e30a5d6994076dddb514f6152
Linder Hassinger <linderhassinger00@gmail.com>
2024-06-17 19:31:26 -0500

    Initial commit

---
+++ test/app.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "@jest/globals";
+import { sumar } from "../src/utils/helper";
+
+describe("primer test", () => {
+  it("sumando numeros", () => {
+    expect(sumar(2, 2)).toBe(4);
+  });
+  
+  it("sumando numeros 2", () => {
+    expect(sumar(2, 3)).toBe(5);
+  });
+});
